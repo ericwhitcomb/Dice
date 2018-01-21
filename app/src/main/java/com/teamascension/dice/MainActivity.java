f9e340bb0776088e360c78017b2667ac5bedb472
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-01-20 18:42:51 -0700

    Added clear text functionality

--- app/src/main/java/com/teamascension/dice/MainActivity.java
+++ app/src/main/java/com/teamascension/dice/MainActivity.java
@@ -2,12 +2,28 @@ package com.teamascension.dice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonClear;
+    EditText editNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editNumber = (EditText) this.findViewById(R.id.editNumber);
+        buttonClear = (Button) this.findViewById(R.id.buttonClear);
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editNumber.setText("");
+            }
+        });
     }
 }
